2ddacb3516d52799a9bf622719e8257448e08813
高鑫崧 <50285788+SongDI911@users.noreply.github.com>
2025-06-18 14:56:35 +0800

    Update flux.py

--- nodes/lora/flux.py
+++ nodes/lora/flux.py
@@ -3,8 +3,8 @@ import logging
 
 import folder_paths
 
-from nunchaku.lora.flux import to_diffusers
-
+#from nunchaku.lora.flux import to_diffusers
+from ...wrappers.flux import to_diffusers
 from ...wrappers.flux import ComfyFluxWrapper
 
 logging.basicConfig(level=logging.INFO)
